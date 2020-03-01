93fdcce269018cf74f295413372fe5e789d1c891
Chris Trinh <christrinh5@gmail.com>
2020-02-29 23:03:26 -0800

    Add sign in form

---
+++ src/pages/session/session.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './session.styles.scss';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const Session = () => (
+  <div className="session">
+  HELLO 
+    <SignIn></SignIn>
+  </div>
+)
+
+export default Session;
