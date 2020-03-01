42e1e2bc253f2d54e60aac96ab9a2082e5408843
Chris Trinh <christrinh5@gmail.com>
2020-02-29 20:15:18 -0800

    Shop Page, Collection item and preview component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATSPAGE</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route path="/shop/hats" component={HatsPage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
       </Switch>
     </div>
-  );
+  )
 }
 
 export default App;
