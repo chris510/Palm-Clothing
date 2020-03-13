8af422dee7a63f287cf2a614dd545555b4aed563
Chris Trinh <christrinh5@gmail.com>
2020-03-12 19:20:41 -0700

    Cart icon dropdown functionality

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { ReactComponent as ShoppingIcon } from '../../shopping-bag.svg'
 import './cart-icon.styles.scss';
 
+import { CartContext } from '../../providers/cart/cart.provider';
+
 const CartIcon: React.FC = () => {
+  const { toggleHidden, cartItemsCount } = useContext(CartContext);
   return (
-    <div className="cart-icon">
+    <div className="cart-icon" onClick={toggleHidden}>
       <ShoppingIcon className="shopping-icon"/>
-      <span className="item-count">0</span>
+      <span className="item-count">{cartItemsCount}</span>
     </div>
   )
 }
