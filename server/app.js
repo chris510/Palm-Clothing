e2f4f5315f12a21e6e284ff962a3ee1c8829c5e9
Chris Trinh <christrinh5@gmail.com>
2020-03-17 19:24:18 -0700

    Stripe payment backend route

--- server/app.js
+++ server/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // const mongoConnect = require('./util/database');
 const mongoose = require("mongoose");
@@ -8,19 +14,43 @@ const usersRoute = require("./routes/api/users.route");
 const db = require("./config/keys").mongoURI;
 
 const app = express();
+const PORT = process.env.port || 5000;
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB..."))
   .catch(err => console.error("Could not connect to MongoDB..."));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({limit: '50mb'}));
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  })
+}
 
 app.use(express.json());
 app.use("/api/users", usersRoute)
 
-const PORT = process.env.port || 5000;
+app.post('./payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  })
+})
+
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
 
 
