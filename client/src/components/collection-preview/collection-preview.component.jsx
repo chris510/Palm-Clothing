d34ecafd9ba624c683ab9c93313e9847a7a25c17
Chris Trinh <christrinh5@gmail.com>
2020-03-30 23:37:40 -0700

    Refactor Collection Preview Container into styled components

--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -3,22 +3,24 @@ import { withRouter } from 'react-router-dom';
 
 import  './collection-preview.styles.scss';
 
+import { CollectionPreviewContainer, CollectionTitleContainer, PreviewContainer } from './collection-preview.styles';
+
 import CollectionItem from '../collection-item/collection-item.component';
 // import CollectionsContext from '../../context/collections/collections.context';
 
 const CollectionPreview = ({ title, items, routeName, match, history }) => (
-  <div className='collection-preview'>
-    <h1 className='title' onClick={() => history.push(`${match.path}/${routeName}`) }>
+  <CollectionPreviewContainer>
+    <CollectionTitleContainer onClick={() => history.push(`${match.path}/${routeName}`) }>
       {title.toUpperCase()}
-    </h1>
-    <div className='preview'>
+    </CollectionTitleContainer>
+    <PreviewContainer>
       {items
         .filter((_, idx) => idx < 4)
         .map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-    </div>
-  </div>
+    </PreviewContainer>
+  </CollectionPreviewContainer>
 );
 
 export default withRouter(CollectionPreview);
---
+++ client/src/components/collection-preview/collection-preview.styles.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const CollectionPreviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 3rem;
+`
+
+export const CollectionTitleContainer = styled.h1`
+  font-size: 3rem;
+  margin-bottom: 2.5rem;
+` 
+
+export const PreviewContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
