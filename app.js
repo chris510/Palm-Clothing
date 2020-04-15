e5d8f3c69b41beaf1e5198899453177a7cd25f32
Chris Trinh <christrinh5@gmail.com>
2020-04-15 16:02:13 -0700

    change backend build route

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '../build/index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   })
 }
 
