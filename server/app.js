700d619a05319bc8df0d141c5c688850dd5f9ed3
Chris Trinh <christrinh5@gmail.com>
2020-04-15 13:45:05 -0700

    Change production build path

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static('../build'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   })
