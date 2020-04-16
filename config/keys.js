f5458620ce91bda7774e7ad1c8e555c0822c2389
Chris Trinh <christrinh5@gmail.com>
2020-04-15 20:52:51 -0700

    Add herokumongo server

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://christrinh:chris123@crwn-clothing1-kozza.mongodb.net/crwn-clothing-1?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://christrinh:chris123@crwn-clothing1-kozza.mongodb.net/crwn-clothing-1?retryWrites=true&w=majority',
+  herokuURI: 'mongodb://christrinh:chris123@ds139939.mlab.com:39939/heroku_q35s14m9'
 }
