06c697e25653653f22a442668dc57a7f622c24a5
Chris Trinh <christrinh5@gmail.com>
2020-04-15 20:58:10 -0700

    Production express server * to /

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'build')));
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   })
 }
